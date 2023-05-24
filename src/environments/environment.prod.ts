ba23d4458ae95afad3535cafb8b59fd07d2a98dc
abigailojeda <abigail.arte@gmail.com>
2023-05-24 23:35:07 +0100

    fix openai

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   token: 'ZmYyZDc4YTEyNzRjNGY4MGJkYjgyYjA0ZDdkYzY4OTM6IERCYzdEdFVBY0FFWjJKeEw5UEtYK2tadUhmZXdETjlpbjljYTkrQ3ZXcHg5N24vdWpo',
   clientId: 'ff2d78a1274c4f80bdb82b04d7dc6893',
   clientSecret: 'DBc7DtUAcAEZ2JxL9PKX+kZuHfewDN9in9ca9+CvWpx97n/ujh',
-  openAiKey:'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'
+
 };
