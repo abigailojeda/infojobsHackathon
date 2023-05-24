9b6cc88938ab1fe8a15941c0a37de8bccbb0f3aa
abigailojeda <abigail.arte@gmail.com>
2023-05-24 23:36:21 +0100

    fix openai

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  token: 'ZmYyZDc4YTEyNzRjNGY4MGJkYjgyYjA0ZDdkYzY4OTM6IERCYzdEdFVBY0FFWjJKeEw5UEtYK2tadUhmZXdETjlpbjljYTkrQ3ZXcHg5N24vdWpo',
-  clientId: 'ff2d78a1274c4f80bdb82b04d7dc6893',
-  clientSecret: 'DBc7DtUAcAEZ2JxL9PKX+kZuHfewDN9in9ca9+CvWpx97n/ujh',
-
-};
